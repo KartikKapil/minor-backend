643fdea0327148b196e8cf0be4a82638ffe52425
Kartik Kapil <kkapil58@gmail.com>
2021-10-02 17:01:03 +0530

    ADD MODELS

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from . models import Notes
+
+admin.site.register(Notes)
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Notes(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    Date_of_entry = models.DateField(auto_now_add=True)
+    Entry = models.TextField()
 
 # Create your models here.
